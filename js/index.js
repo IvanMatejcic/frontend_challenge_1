b7c0c957eef12aa975205e5ac204465cab4815dd
Ivan <ivanmatejcic1@gmail.com>
2022-10-11 19:45:26 +0200

    Further styling.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-//Nothing here yet!
+function myFunction() {
+  var x = document.getElementById("navbar-list");
+  
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myFunction2(){
+    var y = document.getElementByClass("fa-bars")
+
+  if(y.style.display === "none"){
+    x.style.display = "block";
+  }
+  else{
+    x.style.display = "none";
+  }
+}
